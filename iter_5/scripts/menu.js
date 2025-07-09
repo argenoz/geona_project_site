a421c7d4d1bd3b7124b0132f0d9ee3c4b8272789
argenoz <dahnny@yandex.ru>
2025-07-09 14:53:32 +0300

    5th itration

---
+++ iter_5/scripts/menu.js
@@ -0,0 +1,65 @@
+let G=0;
+let i = 0;
+
+function menu__(e)
+	{
+		if(i==0)
+		if(G==0)
+			{
+				i=1;
+				G = new ( function()  {
+				this.d = {};	
+				this.vybor = (n_) => 
+					{
+					let n = n_;
+					let ev = document.getElementById("ekrav_vyvoda");
+					
+					let stro = n_.target.getAttribute('vybor');
+					if(ev.getAttribute("vybor")!==stro)
+					if(this.d[stro]===undefined)
+						{
+						ev.replaceChildren();
+						let xhr = new XMLHttpRequest();
+						xhr.open("GET","articles/"+stro+".json");
+						xhr.send();
+						xhr.onload=() => {
+							i=0;
+								this.d[stro] = JSON.parse(xhr.responseText)['ans'];
+						let evv = document.createElement('div');
+						evv.innerText= this.d[stro];
+						ev.appendChild(evv);
+						ev.setAttribute("vybor",stro);
+						evv.setAttribute('align',"justify");
+						evv.style.top=0;
+						evv.style.left=0;
+						evv.style.position="relative";
+										};	
+						}
+					else
+						{
+							i=0;
+							ev.replaceChildren();
+							let evv = document.createElement('div');
+							evv.innerText= this.d[stro];
+							ev.appendChild(evv);
+							ev.setAttribute("vybor",stro);
+							evv.setAttribute('align',"justify");
+							evv.style.top=0;
+							evv.style.left=0;
+							evv.style.position="relative";
+						}
+					else {ev.setAttribute("vybor",stro); i=0;}
+					};
+				
+				
+				
+				} )();
+			}
+		
+			
+			G.vybor(e);
+			
+		
+	}
+
+
