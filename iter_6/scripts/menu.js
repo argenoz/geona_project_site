739b9f6e32699e99d7231e16a07271853ed59dff
argenoz <dahnny@yandex.ru>
2025-07-10 18:19:46 +0300

    6th iteration_4

--- iter_6/scripts/menu.js
+++ iter_6/scripts/menu.js
@@ -71,10 +71,5 @@ function menu__(e)
 
 
 
-function set_opt()
-	{
-		let g = document.getElementById('ab_auth_des_dev');
-		g.setAttribute('vybor','ADD');
-		g.addEventListener('click',menu__);
-	}
+
 
---
+++ iter_6/scripts/set_opt.js
@@ -0,0 +1,6 @@
+function set_opt()
+	{
+		let g = document.getElementById('ab_auth_des_dev');
+		g.setAttribute('vybor','ADD');
+		g.addEventListener('click',menu__);
+	}
